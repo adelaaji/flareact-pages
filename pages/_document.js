ec92b49cbab4be35b3359d898ffb622becdd518e
Adel <adel.aaji@gmail.com>
2020-12-15 23:11:39 +0300

    .

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,11 @@ import Document, { Html, Head, Main, FlareactScript } from "flareact/document";
 class MyDocument extends Document {
   static async getEdgeProps(ctx) {
     console.log("CX", ctx.event.request);
-    const props = await Document.getEdgeProps(ctx);
+
+    let props = await Document.getEdgeProps(ctx);
+    if (ctx.event.request.cf.country == "TR") {
+      props = "Welcome from turkey";
+    }
     console.log("Pro", props);
     return { ...props };
   }
