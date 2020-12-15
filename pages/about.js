e7c55a97939ed074ba740339817269cf9d4d3cf5
Adel <adel.aaji@gmail.com>
2020-12-15 13:27:07 +0300

    Add ctx

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,30 @@
-export default function Index() {
+export async function getEdgeProps(ctx) {
+  const posts = [
+    { id: 1, title: " 12" },
+    { id: 2, title: " 34" },
+    { id: 3, title: " 56" },
+    { id: 4, title: " 78" },
+    { id: 5, title: " 99" },
+  ];
+
+  return {
+    props: {
+      posts,
+      ctx,
+    },
+  };
+}
+
+export default function About({ posts, ctx }) {
   return (
-    <div style={{ marginTop: "70px" }}>
-      <center>
-        <h1>Welcome to About page</h1>
-      </center>
+    <div>
+      {console.log("ctx", ctx)}
+      <h1>Posts</h1>
+      <ul>
+        {posts.map((post) => {
+          return <li key={post.id}>{post.title}</li>;
+        })}
+      </ul>
     </div>
   );
 }
