c36f73296c5f57c513ecc9e62e53f4012345677d
Adel <adel.aaji@gmail.com>
2020-12-16 21:31:13 +0300

    ..

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
 export default function App({ Component, pageProps }) {
   console.log("Component", Component);
   console.log("pageProps", pageProps);
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <h1> PAGE - </h1>
+      <Component {...pageProps} />
+    </div>
+  );
 }
