06a2ab74912141de40053b4aef86f42eac5e2c44
Adel <adel.aaji@gmail.com>
2020-12-16 23:26:27 +0300

    ...

--- pages/_document.js
+++ pages/_document.js
@@ -1,23 +1,14 @@
 import Document, { Html, Head, Main, FlareactScript } from "flareact/document";
-import { getComent } from "./functions";
 
 class MyDocument extends Document {
   static async getEdgeProps(ctx) {
-    console.log("CX", ctx.event.request);
-    let comment = await getComent(12);
     let props = await Document.getEdgeProps(ctx);
     if (ctx.event.request.cf.country == "TR") {
-      comment = await getComent(12);
+      props.html = `Welcome from turkey ${ctx.event.request.cf.city}`;
     } else {
-      comment = await getComent(5);
+      props.html += `<h1>${ctx.event.request.cf.city}</h1>`;
     }
-    props.html += `<h1>
-    <pre>
-      <code>
-        ${JSON.stringify(comment, 0, 5)}
-      </code>
-    </pre>
-    </h1>`;
+
     console.log("Pro", props);
     return { ...props };
   }
