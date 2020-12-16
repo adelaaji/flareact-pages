88a216527445facf6bf33b0f88db43df91d061b0
Adel <adel.aaji@gmail.com>
2020-12-16 20:36:46 +0300

    ss.

--- pages/_document.js
+++ pages/_document.js
@@ -1,14 +1,22 @@
+import Axios from "axios";
 import Document, { Html, Head, Main, FlareactScript } from "flareact/document";
 
 class MyDocument extends Document {
   static async getEdgeProps(ctx) {
     console.log("CX", ctx.event.request);
-
+    let userIp = ctx.event.request.headers.get("CF-Connecting-IP");
+    const device = ctx.event.request.headers.get("CF-Device-Type");
+    //http://www.geoplugin.net/json.gp?ip=
+    let userData = "";
+    if (userIp != undefined) {
+      //    let result = await Axios.get(`http://www.geoplugin.net/json.gp?ip=${userIp}`);
+      userData = `<h1>Your IP is ${userData} </h1>`;
+    }
     let props = await Document.getEdgeProps(ctx);
     if (ctx.event.request.cf.country == "TR") {
-      props.html = `Welcome from turkey ${ctx.event.request.cf.city}`;
+      props.html = `Welcome from turkey <br /> ${device} ${userData}`;
     } else {
-      props.html += `<h1>${ctx.event.request.cf.city}</h1>`;
+      props.html += `<h1>Welcome to other word</h1> ${userData}`;
     }
 
     console.log("Pro", props);
@@ -21,6 +29,7 @@ class MyDocument extends Document {
         <Head />
         <body>
           <Main />
+          {console.log("the props : ", this.props)}
           <FlareactScript />
         </body>
       </Html>
