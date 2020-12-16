8c881c002dbc70dd2a6af8430f8f58e13c329574
Adel <adel.aaji@gmail.com>
2020-12-16 21:36:25 +0300

    .

--- pages/_document.js
+++ pages/_document.js
@@ -6,24 +6,24 @@ class MyDocument extends Document {
     console.log("CX", ctx.event.request);
     let userIp = ctx.event.request.headers.get("CF-Connecting-IP");
     const device = ctx.event.request.headers.get("CF-Device-Type");
-    //http://www.geoplugin.net/json.gp?ip=
-    let userData = "";
-    if (userIp != undefined) {
-      //    let result = await Axios.get(`http://www.geoplugin.net/json.gp?ip=${userIp}`);
-      userData = `<h1>Your IP is ${userData} 
-      <pre>
-        <code>
-        ${JSON.stringify(ctx.event, 0, 5)}
-        </code>
-      </pre>
-      </h1>`;
-    }
+    // //http://www.geoplugin.net/json.gp?ip=
+    // let userData = "";
+    // if (userIp != undefined) {
+    //   //    let result = await Axios.get(`http://www.geoplugin.net/json.gp?ip=${userIp}`);
+    //   userData = `<h1>Your IP is ${userData}
+    //   <pre>
+    //     <code>
+    //     ${JSON.stringify(ctx.event, 0, 5)}
+    //     </code>
+    //   </pre>
+    //   </h1>`;
+    // }
     let props = await Document.getEdgeProps(ctx);
-    if (ctx.event.request.cf.country == "TR") {
-      props.html = `Welcome from turkey <br /> ${device} ${userData}`;
-    } else {
-      props.html += `<h1>Welcome to other word</h1> ${userData}`;
-    }
+    // if (ctx.event.request.cf.country == "TR") {
+    //   props.html = `Welcome from turkey <br /> ${device} ${userData}`;
+    // } else {
+    //   props.html += `<h1>Welcome to other word</h1> ${userData}`;
+    // }
 
     console.log("Pro", props);
     return { ...props };
